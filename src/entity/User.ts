69376c1875befa7df050b820a64b6d88247749a4
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-21 11:13:37 +0300

    new project

---
+++ src/entity/User.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+enum role {
+    ADMIN = "admin",
+    USER = "user",
+    CUSTOMER = "customer",
+}
+
+enum confirmed {
+    PENDING = "pending",
+    EMAIL = "email",
+    DENIED = "denied",
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 100 })
+  firstName: string;
+
+  @Column({ type: "varchar", length: 100 })
+  lastName: string;
+
+  @Column({ unique: true, type: "varchar", length: 100 })
+  email: string;
+
+  @Column({ type: "varchar", length: 100 })
+  password: string;
+
+  @Column({type: "enum", enum: role, default: role.USER})
+  role: role;
+
+  @Column({type: "enum", enum: confirmed, default: confirmed.PENDING})
+  confirmed: confirmed;
+}
