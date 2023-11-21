07a3002d27c268ea2c44ed73a4fa08ea5e9eae41
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-21 12:29:37 +0300

    Refactor Email and User entities

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import { Phone } from "./Phone";
 import { Email } from "./Email";
 import { Address } from "./Address";
 
 enum role {
-    ADMIN = "admin",
-    USER = "user",
-    CUSTOMER = "customer",
+  ADMIN = "admin",
+  USER = "user",
+  CUSTOMER = "customer",
 }
 
 enum confirmed {
-    PENDING = "pending",
-    EMAIL = "email",
-    DENIED = "denied",
+  PENDING = "pending",
+  EMAIL = "email",
+  DENIED = "denied",
 }
 
 @Entity("users")
@@ -32,10 +32,10 @@ export class User {
   @Column({ type: "varchar", length: 100 })
   password: string;
 
-  @Column({type: "enum", enum: role, default: role.USER})
+  @Column({ type: "enum", enum: role, default: role.USER })
   role: role;
 
-  @Column({type: "enum", enum: confirmed, default: confirmed.PENDING})
+  @Column({ type: "enum", enum: confirmed, default: confirmed.PENDING })
   confirmed: confirmed;
 
   @OneToMany(() => Phone, (phone) => phone.user, { cascade: true })
@@ -46,4 +46,10 @@ export class User {
 
   @OneToMany(() => Address, (address) => address.user, { cascade: true })
   addresses: Address[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
