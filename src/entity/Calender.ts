449519d478a314554aedcad021f919355daa3173
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-21 12:46:04 +0300

    Refactor Calender entity in src/entity/Calender.ts

--- src/entity/Calender.ts
+++ src/entity/Calender.ts
@@ -1,26 +1,25 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    JoinColumn,
-  } from "typeorm";
-  import { User } from "./User";
-  
-  enum calenderType {
-    MEETING = "meeting",
-  }
-  
-  @Entity("calenders")
-  export class Calender {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column({ type: "enum", enum: calenderType, default: calenderType.MEETING })
-    calenderType: calenderType;
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./User";
 
-    @ManyToOne(() => User, (user) => user.id)
-    @JoinColumn({ name: "user_id" })
-    user: User;
-  }
-  
+enum calenderType {
+  MEETING = "meeting",
+}
+
+@Entity("calenders")
+export class Calender {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "enum", enum: calenderType, default: calenderType.MEETING })
+  calenderType: calenderType;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+}
