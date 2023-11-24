e173cde41df7b91f95aa8a0123278bc1f45523ce
Talha Seven <131260032+TalhaSeven@users.noreply.github.com>
2023-11-24 11:26:24 +0300

    Update database configuration in data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,12 +11,12 @@ import { Log } from "./entity/Log";
 require("dotenv").config();
 
 export const AppDataSource = new DataSource({
-  type: "mysql",
+  type: process.env.DB_TYPE as any,
   host: process.env.DB_HOST,
-  port: 3306,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   synchronize: true, // live in false in production.
   logging: false,
   entities: [User, Phone, Email, Address, Calender, UserInfo, Log],
